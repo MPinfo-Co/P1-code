a7ad59f56fc77de00ef93cc55cf6cefba7528490
Yin Chen (陳敬尹)-倍力資訊 <MP0458@mpinfo.com.tw>
2026-05-06 13:53:27 +0800

    fix: switch alembic.ini's database.url from hard coded to read .env file

--- backend/bpBoxAlembic/env.py
+++ backend/bpBoxAlembic/env.py
@@ -5,6 +5,7 @@ from sqlalchemy import pool
 
 from alembic import context
 
+from app.config.settings import settings
 from app.db.models import base
 from app.db import models
 
@@ -12,6 +13,7 @@ from app.db import models
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
+config.set_main_option("sqlalchemy.url", settings.database_url)
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
